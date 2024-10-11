2a67a79182c664f1c3266d18457b5b9e38cb50a8
Igor <dumanschi.igor@gmail.com>
2024-10-11 17:29:09 +0300

    JS_5_HW

---
+++ main.js
@@ -0,0 +1,74 @@
+const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+// const addTow = (num) => {
+// 	return num + 2
+// }
+
+// const list1 = numbers.map(addTow)
+// console.log(list1)
+
+
+
+
+// const list2 = list1.forEach(function(item, index) {
+// 	console.log(item, index)}
+// )
+
+// const inmultire = (num) => {
+// 	return num * 2
+// }
+
+// const list3 = numbers.map(inmultire)
+// console.log(list3)
+
+// const inpartire = (num) => {
+// 	return num / 2
+// }
+
+// const list4 = numbers.map(inpartire)
+// console.log(list4)
+
+// const puteraAdoua = (num) => {
+// 	return num **2
+// }
+
+// const list5 = numbers.map(puteraAdoua)
+// console.log(list5)
+
+
+
+const fruits = ['cirese', 'visine', 'mere', 'caise', 'prune', 'banane']
+
+// const fruits2 = fruits.forEach(function (item, index) {
+// 	console.log(item, index)
+// })
+
+// const fruits3 = fruits.forEach(function (item, index) {
+// 	console.log(item + index)
+// })
+
+
+// const fristLetter = fruits.forEach(function(item) {
+// 	console.log(item[0])
+// })
+// const lasttLetter = fruits.forEach(function(item) {
+// 	console.log(item[item.length -1])
+// })
+
+
+
+// const listOne = [1, 2, 3, 4, 5]
+// const listtwo = [6, 7, 8, 9, 10]
+
+// const finalList	= [...listOne, ...listtwo]
+// console.log(finalList)
+
+// const driver = {
+// 	name: 'Ion',
+// 	age: '40',
+// 	city: 'Chicago'
+// }
+
+// const anotherDriver = {...driver, name: 'Vasile'}
+
+// console.log(anotherDriver)
